961810042c853572b77fd66554930c83a4b0e3d3
Al Sayer <alsayerhaitham@gmail.com>
2024-04-11 15:49:31 +0200

    video client

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_clone/env/env.dart';
 import 'package:youtube_clone/screens/search_screen.dart';
 import 'package:youtube_clone/screens/shorts_screen.dart';
 import 'package:youtube_clone/screens/mux_shorts_screen.dart';
+import 'package:youtube_clone/services/video_client.dart';
 import 'package:youtube_clone/shared/theme.dart';
 
-void main() {
+Future<void> main() async {
+  final videoClient  = VideoClient();
+  final muxAssets = await videoClient.getMuxAssets();
+  print(muxAssets);
   runApp(const MainApp());
 }
 
@@ -16,12 +21,12 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Youtube clone',
       theme: const AppTheme().themeData,
-      initialRoute: '/shorts',
+      initialRoute: '/search',
       routes: {
         '/shorts': (context) => const ShortsScreen(),
         '/search': (context) => const SearchScreen(),
       },
-      home: ShortsScreen(),
+      home: SearchScreen(),
     );
   }
 }
